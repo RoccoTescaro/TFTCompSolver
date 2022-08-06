83091eb20611c04777ecb1e9a2ec2b7e13f10414
Rocco Tescaro <rocco.tescaro@stud.unifi.it>
2022-08-06 04:42:13 +0200

    comp generation done. started debug of weight function and entropy comp

--- data.js
+++ data.js
@@ -1,102 +1,520 @@
 const data = {
-    "Champions" :
-    [
+    Champions :
+    {
+        Aatrox : 
         {
-            "name" : "Atrox",
-            "image" : "fillthiswithdotpngfile",
+
             "class" : ["Shimmerscale" , "Warrior"],
             "items" : ["Gargoyle Stoneplate", "Sunfire Cape", "Warmog's Armor"],
             "cost" : 1
         },
 
+        Anivia : 
         {
-            "name" : "Anivia",
-            "image" : "fillthiswithdotpngfile",
+
             "class" : ["Jade" , "Evoker", "Legend"],
             "items" : ["Archangel's Staff", "Morellonomicon", "Spear of Shojin"],
             "cost" : 3
         },
 
+        AoShin : 
         {
-            "name" : "Ao Shin",
-            "image" : "fillthiswithdotpngfile",
+
             "class" : ["Tempest" , "Dragon"],
             "items" : ["Archangel's Staff", "Hextech Gunblade", "Spear of Shojin"],
             "cost" : 10
         },        
 
+        Ashe : 
         {
-            "name" : "Ashe",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Jade" , "Dragonmancer", "Swiftshot"],
             "items" : ["Guinsoo's Rageblade", "Spear of Shojin", "Statikk Shiv"],
             "cost" : 2
         },      
 
+        AurelionSol :
         {
-            "name" : "Aurelion Sol",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Astral", "Evoker" , "Dragon"],
             "items" : ["Archangel's Staff", "Hextech Gunblade", "Spear of Shojin"],
             "cost" : 10
         },  
 
+        Bard :
         {
-            "name" : "Bard",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Guild" , "Mystic", "Bard"],
             "items" : ["Chalice of Power", "Zeke's Herald", "Zephyr"],
             "cost" : 5
         },  
 
+        Braum :
         {
-            "name" : "Braum",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Scalescorn" , "Guardian"],
             "items" : ["Gargoyle Stoneplate", "Sunfire Cape", "Warmog's Armor"],
             "cost" : 2
         },  
 
+        Corki :
         {
-            "name" : "Corky",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Revel" , "Cannoneer"],
             "items" : ["Guinsoo's Rageblade", "Spear of Shojin", "Statikk Shiv"],
             "cost" : 4
         },  
 
+        Daeja :
         {
-            "name" : "Daeja",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Mirage" , "Dragon"],
             "items" : ["Giant Slayer", "Guinsoo's Rageblade", "Quicksilver"],
             "cost" : 8
         },  
 
+        Diana :
         {
-            "name" : "Diana",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Scalescorn", "Assassin"],
             "items" : ["Frozen Heart", "Ionic Spark", "Sunfire Cape"],
             "cost" : 3
         },  
 
+        Elise :
         {
-            "name" : "Elise",
-            "image" : "fillthiswithdotpngfile",
+            
             "class" : ["Whispers" , "ShapeShifter"],
             "items" : ["Bloodthirster", "Guinsoo's Rageblade", "Quicksilver"],
             "cost" : 3
-        }  
-    ],
+        },  
 
-    "Items" :
-    [
+        Ezreal :
+        {
+            
+            "class" : ["Tempest" , "Swiftshot"],
+            "items" : ["Blue Buff", "Infinity Edge", "Jeweled Gauntlet"],
+            "cost" : 1
+        }, 
 
-    ],
+        Gnar :
+        {
+            
+            "class" : ["Jade" , "ShapeShifter"],
+            "items" : ["Gargoyle Stoneplate", "Sunfire Cape", "Warmog's Armor"],
+            "cost" : 2
+        },  
 
-    "Traits" :
-    [
+        Hecarim :
+        {
+            
+            "class" : ["Ragewing" , "Cavalier"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        }, 
+
+        Heimerdinger :
+        {
+            
+            "class" : ["Trainer" , "Mage"],
+            "items" : ["Infinity Edge", "Jeweled Gauntlet", "Spear of Shojin"],
+            "cost" : 1
+        }, 
+
+        Idas :
+        {
+            
+            "class" : ["Shimmerscale" , "Dragon", "Guardian"],
+            "items" : ["Dragon's Claw", "Gargoyle Stoneplate", "Warmog's Armor"],
+            "cost" : 8
+        }, 
+
+        Illaoi :
+        {
+            
+            "class" : ["Astral" , "Bruiser"],
+            "items" : ["Gargoyle Stoneplate", "Sunfire Cape", "Warmog's Armor"],
+            "cost" : 3
+        }, 
+
+        Jinx :
+        {
+            
+            "class" : ["Revel" , "Cannoneer"],
+            "items" : ["Guinsoo's Rageblade", "Spear of Shojin", "Statikk Shiv"],
+            "cost" : 2
+        },  
+
+        Karma :
+        {
+            
+            "class" : ["Jade" , "Dragonmancer"],
+            "items" : ["Blue Buff", "Infinity Edge", "Jeweled Gauntlet"],
+            "cost" : 1
+        },  
+
+        Kayn :
+        {
+            
+            "class" : ["Ragewing" , "Shimmerscale", "Assassin"],
+            "items" : ["Bloodthirster", "Infinity Edge", "Last Whisper"],
+            "cost" : 2
+        },  
+
+        LeeSin :
+        {
+            
+            "class" : ["Tempest" , "Dragonmancer"],
+            "items" : ["Hand of Justice", "Infinity Edge", "Jeweled Gauntlet"],
+            "cost" : 3
+        },  
+
+        Leona :
+        {
+            
+            "class" : ["Mirage" , "Guardian"],
+            "items" : ["Gargoyle Stoneplate", "Sunfire Cape", "Warmog's Armor"],
+            "cost" : 1
+        }, 
+
+        Lillia :
+        {
+            
+            "class" : ["Scalescorn" , "Cavalier", "Mage"],
+            "items" : ["Chalice of Power", "Zeke's Herald", "Zephyr"],
+            "cost" : 2
+        },  
+
+        Lulu :
+        {
+            
+            "class" : ["Trainer" , "Mystic", "Evoker"],
+            "items" : ["Chalice of Power", "Zeke's Herald", "Zephyr"],
+            "cost" : 3
+        },  
+
+        Nami :
+        {
+            
+            "class" : ["Astral" , "Mage", "Mystic"],
+            "items" : ["Archangel's Staff", "Spear of Shojin", "Statikk Shiv"],
+            "cost" : 2
+        },  
+
+        Neeko :
+        {
+            
+            "class" : ["Jade" , "ShapeShifter"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        },
         
-    ]
+        Nidalee : ///#TODO FIX ITEMS from here
+        {
+            
+            "class" : ["Astral" , "ShapeShifter"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Nunu :
+        {
+            
+            "class" : ["Mirage" , "Cavalier"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        Olaf :
+        {
+            
+            "class" : ["Scalescorn" , "Bruiser", "Warrior"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        Ornn :
+        {
+            
+            "class" : ["Tempest" , "Bruiser", "Legend"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        },
+
+        Pyke :
+        {
+            
+            "class" : ["Whispers" , "Assassin"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 5
+        },
+
+        Qiyana :
+        {
+            
+            "class" : ["Tempest" , "Assassin"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+
+        Ryze :
+        {
+            
+            "class" : ["Guild" , "Mage"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+        
+        Sejuani :
+        {
+            
+            "class" : ["Guild" , "Cavalier"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Senna :
+        {
+            
+            "class" : ["Ragewing" , "Cannoneer"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Sett :
+        {
+            
+            "class" : ["Ragewing" , "Dragonmancer"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Shen :
+        {
+            
+            "class" : ["Ragewing" , "Bruiser", "Warrior"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+
+        ShiOhYu :
+        {
+            
+            "class" : ["Jade" , "Mystic", "Dragon"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 8
+        },
+
+        Shyvana :
+        {
+            
+            "class" : ["Ragewing" , "ShapeShifter", "Dragon"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 10
+        },
+
+        Skarner :
+        {
+            
+            "class" : ["Astral" , "Bruiser"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Sona :
+        {
+            
+            "class" : ["Revel" , "Evoker"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        },
+
+        Soraka :
+        {
+            
+            "class" : ["Jade" , "Starcaller"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 5
+        },
+
+        Swain :
+        {
+            
+            "class" : ["Ragewing" , "Dragonmancer", "ShapeShifter"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        Syfen :
+        {
+            
+            "class" : ["Whispers" , "Bruiser", "Dragon"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 8
+        },
+
+        Sylas :
+        {
+            
+            "class" : ["Whispers" , "Mage", "Bruiser"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        TahmKench :
+        {
+            
+            "class" : ["Revel" , "Bruiser"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+        
+        Talon :
+        {
+            
+            "class" : ["Guild" , "Assassin"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        },
+        
+        Taric :
+        {
+            
+            "class" : ["Jade" , "Guardian"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+        
+        Thresh :
+        {
+            
+            "class" : ["Whispers" , "Guardian"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+        
+        Tristana :
+        {
+            
+            "class" : ["Trainer" , "Cannoneer"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+        
+        Twitch :
+        {
+            
+            "class" : ["Guild" , "Swiftshot"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+        
+        Varus :
+        {
+            
+            "class" : ["Astral" , "Swiftshot"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        Vladimir :
+        {
+            
+            "class" : ["Astral" , "Mage"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 1
+        },
+
+        Volibear :
+        {
+            
+            "class" : ["Shimmerscale" , "Dragonmancer", "Legend"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 3
+        },
+
+        Xayah :
+        {
+            
+            "class" : ["Ragewing" , "Swiftshot"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 4
+        },
+
+        Yasuo :
+        {
+            
+            "class" : ["Mirage" , "Dragonmancer", "Warrior"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 5
+        },
+
+        Yone :
+        {
+            
+            "class" : ["Mirage" , "Warrior"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 2
+        },
+
+        Zoe :
+        {
+            
+            "class" : ["Shimmerscale" , "Spell_Thief", "Mage"],
+            "items" : ["Gargoyle Stoneplate", "Morellonomicon", "Warmog's Armor"],
+            "cost" : 5
+        }
+
+    },
+
+    Items :
+    {
+
+    },
+
+    Traits :
+    {
+        Astral : [3,6,9],
+        Guild : [1,2,3,4,5,7],
+        Jade : [3,6,9,12],
+        Mirage : [2,4,6,8],
+        Ragewing : [3,6,9],
+        Revel : [2,3,4,5],
+        Scalescorn : [2,4,6],
+        Shimmerscale : [3,5,7,9],
+        Tempest : [2,4,6,8],
+        Trainer : [2,3],
+        Whispers : [2,4,6,8],
+        Assassin : [2,4,6],
+        Bard : [1],
+        Bruiser : [2,4,6,8],
+        Cannoneer : [2,3,4,5],
+        Cavalier : [2,3,4,5],
+        Dragon : [1],
+        Dragonmancer : [3,6,9],
+        Evoker : [2,4,6],
+        Guardian : [2,4,6],
+        Legend : [3],
+        Mage : [3,5,7,9],
+        Mystic : [2,3,4,5],
+        ShapeShifter : [2,4,6],
+        Spell_Thief : [1],
+        Starcaller : [1],
+        Swiftshot : [2,4,6],
+        Warrior : [2,4,6]  
+    },
+
+    Percentages : 
+    {
+        1 : [100,0,0,0,0],
+        2 : [75,25,0,0,0],
+        3 : [55,30,15,0,0],
+        4 : [45,33,20,2,0],
+        5 : [25,40,30,5,0],
+        6 : [19,30,35,15,1],
+        7 : [16,20,35,25,4],
+        8 : [9,15,30,30,16],
+        9 : [5,10,20,40,25],
+        10 : [1,2,12,50,35]
+    }
 };
--- script.js
+++ script.js
@@ -1,82 +1,225 @@
-function waveFunction(data_, length_, comp_)
+class CompSolver 
 {
-    if(comp_ == undefined)
-        comp_ = new Array(length_);
-    else if(comp_.length < length_)
-        for(var i = 0; i < length_-comp_.length; i++)
-            comp_.push();
+    level;
 
-    for(var i = 0; i < length_; i++)
+    lockedComp;
+    activeTraits;
+    
+    nDragons;
+
+    #allChampions;
+    entropyComp;
+
+    constructor(level_, lockedComp_ = new Set()) 
     {
-        if(comp_[i] == undefined)
+        this.level = level_;
+
+        this.lockedComp = new Set(lockedComp_);
+        this.activeTraits = this.traits(this.lockedComp);
+
+        this.nDragons = 0;
+        for(let champ of this.lockedComp)
+            if(data.Champions[champ].class.indexOf('Dragon') != -1)
+                this.nDragons++;
+
+        this.#allChampions = new Set();
+        let traits_ = this.activeTraits;
+        for(let champion in data.Champions)
+            this.#allChampions.add(champion);
+        this.updateEntropyComp(traits_);
+
+    }
+
+    traits(comp_)
+    {
+    let traits_ = new Object();
+
+    for(const champ of comp_)
+        for(const trait of data.Champions[champ].class)
         {
-            comp_[i] = new Array();
-            for(var j = 0; j < data_.Champions.length; j++)
-                comp_[i].push(data_.Champions[j].name);
+            if(traits_[trait] == undefined) traits_[trait] = 1;
+            else traits_[trait] += 1;
+
+            if(trait == 'Dragon')
+                traits_[data.Champions[champ].class[0]] += 2;
+            }
+
+    return traits_;
+    }
+
+    entropy(index_)
+    {
+    let sumWeights_ = 0;
+    let sumWeightsLog_ = 0;
+
+    for(const elm of this.entropyComp[index_])
+    {
+        let weight_ = this.weight(elm);
+        sumWeights_ += weight_;
+        sumWeightsLog_ += weight_*Math.log(weight_);
+    }
+
+    return Math.log(sumWeights_)-(sumWeightsLog_) / sumWeights_;
+    }
+
+    weight(elm_)
+    {
+        let cost_ = data.Champions[elm_].cost;
+        if(cost_ > 5) cost_ /= 2;
+
+        let percentage_ = data.Percentages[this.level][cost_-1]/100;
+        
+        let commonTraits_ = this.intersection(new Set(Object.keys(this.activeTraits)),data.Champions[elm_].class);
+        let result = 1;
+        for(let trait of commonTraits_)
+        {
+            let newValue = this.activeTraits[trait]+1;
+            let index_ = -1;
+            for(let level of data.Traits[trait])
+                if(newValue > level)
+                    index_++;
+            if(index_ != -1)
+                result = newValue*((1+index_)/data.Traits[trait].length);
+        }
+
+        return percentage_*(result+cost_);  
+    }
+
+    bestEntropy()
+    {
+    let index_ = 0;
+    let bestEntropy_ = 999999;
+    for(let i = 0; i < this.entropyComp.length; i++)
+    {
+        let eval_ = this.entropy(i);
+        if(eval_ == bestEntropy_)
+        {
+            let rand = Math.random();
+            if(rand > 0.5)
+                index_ = i;
+        }
+        else if(eval_ < bestEntropy_)
+        {
+            bestEntropy_ = eval_;
+            index_ = i;
         }
     }
-
-    return comp_;
-}
-
-function entropy(comp_,square_)
-{
-    var sumWeights = 0;
-    var sumWeightsLog = 0;
-
-    for(var i = 0; i < comp_[square_].length; i++)
-    {
-        var weight_ = weight(comp_, square_, i)
-        sumWeights += weight_;
-        sumWeightsLog += weight_*Math.log(weight_);
+    return index_;
     }
 
-    return Math.log(sumWeights)-(sumWeightsLog) / sumWeights;
-}
-
-function weight(comp_, square_, trait_)
-{
-    return 1; 
-    //#TODO weight based on:
-    //- level and champ cost
-    //- activation of traits 
-}
-
-function square(comp_)
-{
-    var index = 0;
-    var bestEntropy = 999999;
-    for(var i = 0; i < comp_.length; i++)
+    collapse()
     {
-        var eval = entropy(comp_, i);
-        if(eval < bestEntropy && eval > 0) // if eval == 0 function is already collapsed.
-        //a good idea would be to separate wave function and the collapsed one
-        //to speed up calulations.
+        let list_ = this.bestEntropy();
+        let pick_ = undefined;
+        var weight_ = -1;
+        for(let champ of this.entropyComp[list_])
         {
-            bestEntropy = eval;
-            index = i;
+            var squareW = this.weight(champ); 
+            //console.log(champ,squareW);
+            if(weight_ == squareW)
+            {
+                let rand = Math.random();
+                if(rand > 0.5)
+                    pick_ = champ;
+            }
+            else if(weight_ < squareW)
+            {
+                weight_ = squareW;
+                pick_ = champ;
+            }
         }
+    
+        console.log(pick_);
+        this.lockedComp.add(pick_);
+
+        if(data.Champions[pick_].class.indexOf('Dragon') != -1)
+            this.nDragons++;
+
+        let traits_ = this.traits(this.lockedComp);
+        this.activeTraits = traits_;
+        
+        this.updateEntropyComp(traits_);
+
+        if(this.lockedComp.size < this.level-this.nDragons)
+            this.collapse();
     }
-    return index;
-}
 
-function collapse(data_, comp_)
-{
-    var square_ = square(comp_);
-
-    var pick;
-    var weight_ = 0;
-    for(var i = 0; i < comp_[square_].length; i++)
+    union(setA, setB) 
     {
-        var squareW = weight(comp_[square_][i]); 
-        if(weight_ < squareW)
-        {
-            weight_ = squareW;
-            pick = comp_[square_][i];
+        const _union = new Set(setA);
+        for (const elem of setB) {
+        _union.add(elem);
         }
+        return _union;
     }
 
-    comp_[square_] = [pick];
+    intersection(setA, setB)
+    {
+        const _intersection = new Set();
+        for (const elem of setB) {
+            if (setA.has(elem)) {
+                _intersection.add(elem);
+            }
+        }
+        return _intersection;
+    }
 
-    //updates with rules
+    strictRule(traits_, elm_)
+    {
+        for(const trait of data.Champions[elm_].class)
+            if(new Set([...(data.Traits[trait])]).has(traits_[trait]+1))
+                return true;
+        return false;
+    }
+
+    lightRule(traits_, elm_)
+    {
+        for(const trait of data.Champions[elm_].class)
+            if(traits_[trait] != undefined)
+                return true;
+        return false;
+    }
+
+    updateEntropyComp(traits_) //change
+    {
+        this.entropyComp = new Array();
+        for(let i = 0; i < Object.keys(this.activeTraits).length; i++)
+        {
+            let linkedTraitsComp_ = Array.from(this.#allChampions);
+            this.entropyComp.push(new Set(linkedTraitsComp_.filter(elm => 
+                !this.lockedComp.has(elm) && this.lightRule(traits_,elm)
+            )));
+            if(this.entropyComp[i].length == 0)
+                this.entropyComp[i] = this.#allChampions;
+        }
+        if(this.entropyComp.length == 0)
+            this.entropyComp.push(this.#allChampions);
+    }
 }
+
+
+function update(comp_)
+{
+    let container = document.getElementById("container");
+    container.innerHTML = '';
+    for(let champ of comp_.lockedComp)
+    {
+        let item = document.createElement("div");
+        item.classList = "item";
+        item.style.backgroundImage = "url(https://rerollcdn.com/characters/Skin/7/"+champ+".png)";
+        item.title = champ;
+        container.appendChild(item);
+    }
+}
+
+window.onload = (event) => {
+    let comp = new CompSolver(8);
+    comp.collapse();
+    for(let trait of Object.keys(comp.activeTraits))
+    {
+        let value = comp.activeTraits[trait];
+        if(data.Traits[trait].indexOf(value) != -1)
+            console.log(trait, value);
+    }
+    update(comp);
+};
